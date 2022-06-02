a2700a8b3130508376dbec78de76cdebfacaec0f
kouhei <kou19930504@gmail.com>
2022-06-02 18:47:07 +0900

    commit

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-import './style.css'
-import * as THREE from 'three';
-const canvas = document.querySelector('.mycanvas');
+import "./style.css";
+import * as THREE from "three";
+import background from './bg/bg.jpg';
+import { TextureLoader } from "three";
+
+const canvas = document.querySelector(".mycanvas");
 const sizes = {
   width: innerWidth,
-  height: innerHeight
-}
+  height: innerHeight,
+};
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45,sizes.width / sizes.height,0.1,100);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 const renderer = new THREE.WebGLRenderer({
-  canvas:canvas
-})
+  canvas: canvas,
+});
 renderer.setClearColor(0x000000);
-renderer.setSize(sizes.width,sizes.height);
+renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.render(scene,camera);
+const sceneBg = new TextureLoader().load(background);
+scene.background = sceneBg;
+const tick = function(){
+  requestAnimationFrame(tick);
+  renderer.render(scene, camera);
+}
+tick();
+
+const onResize = function () {
+  sizes.width = innerWidth;
+  sizes.height = innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(window.devicePixelRatio);
+};
+window.addEventListener("resize", onResize);

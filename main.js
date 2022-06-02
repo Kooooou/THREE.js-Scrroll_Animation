7e845642d153d73251541ed739175700ae76dfbd
kouhei <kou19930504@gmail.com>
2022-06-02 11:28:19 +0900

    commit

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from 'three';
 const canvas = document.querySelector('.mycanvas');
+const sizes = {
+  width: innerWidth,
+  height: innerHeight
+}
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45,sizes.width / sizes.height,0.1,100);
+const renderer = new THREE.WebGLRenderer({
+  canvas:canvas
+})
+renderer.setClearColor(0x000000);
+renderer.setSize(sizes.width,sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.render(scene,camera);
